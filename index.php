3d4380f1516d45181f8e17b46ed2164b4124fc5e
amna_sadiq <amnasadiq0073@gmail.com>
2020-12-23 03:03:07 +0500

    uni mng

--- index.php
+++ index.php
@@ -25,8 +25,42 @@
 </head>
 <body>
   <!-- menu -->
-  <?php include("includes/parts/menu.php"); ?>
+  <section class="menu cid-rjmzhNCTQQ" once="menu" id="menu2-0">
 
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="index.php">
+                        <img src="assets/images/aschoolicon-122x122.png" alt="University Management System logo image" title="University Management System logo image" style="height: 3.8rem;">
+                    </a>
+                </span>
+                
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item">
+                    <a class="nav-link link text-black display-4" href="index.php"><span class="mbrib-home mbr-iconfont mbr-iconfont-btn"></span>Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link text-black display-4" href="portal.php"><span class="mbri-touch mbr-iconfont mbr-iconfont-btn"></span>Online University Management</a>
+                </li><li class="nav-item"><a class="nav-link link text-black display-4" href="contact.php"><span class="mbri-mobile mbr-iconfont mbr-iconfont-btn"></span>Contact Us</a></li></ul>
+            
+        </div>
+    </nav>
+</section>
+
+<!-- body -->
 <section class="engine"><a href="https://mobirise.me/r">bootstrap template</a></section><section class="cid-rjmziudFpC mbr-parallax-background" id="header2-1">
 
     
@@ -54,7 +88,54 @@
 
 <!-- footer -->
 
-<?php include('includes/parts/footer.php'); ?>
+<section once="" class="cid-rjmzlA1GxG" id="footer7-2">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="row row-links">
+                <ul class="foot-menu">
+                    
+                    
+                    
+                    
+                    
+                <li class="foot-menu-item mbr-fonts-style display-7"><a href="contact.php" target="_blank">Contact&nbsp;Us</a>
+                    </li><li class="foot-menu-item mbr-fonts-style display-7"><a href="portal.php">University Management</a></li><li class="foot-menu-item mbr-fonts-style display-7"></li><li class="foot-menu-item mbr-fonts-style display-7"></li><li class="foot-menu-item mbr-fonts-style display-7">&nbsp;</li></ul>
+            </div>
+            <div class="row social-row">
+                <div class="social-list align-right pb-2">
+                    
+                    
+                    
+                    
+                    
+                    
+                <div class="soc-item">
+                        <a href="https://twitter.com/mobirise" target="_blank">
+                            <span class="socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://www.facebook.com/pages/Mobirise/1616226671953247" target="_blank">
+                            <span class="socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                    </div><div class="soc-item">
+                        <a href="https://www.youtube.com/c/mobirise" target="_blank">
+                            <span class="socicon-youtube socicon mbr-iconfont mbr-iconfont-social"></span>
+                        </a>
+                    </div></div>
+            </div>
+            <div class="row row-copirayt">
+                <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">
+                    © Copyright 2019 University management - All Rights Reserved
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
 
   <script src="assets/web/assets/jquery/jquery.min.js"></script>
   <script src="assets/popper/popper.min.js"></script>
